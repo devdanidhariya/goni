722141775021b40797b72f9bfad39080e2981b0d
devdanidhariya <devdanidhariya@gmail.com>
2021-08-09 18:19:04 +0530

    update portfolio

--- single-page-design.php
+++ single-page-design.php
@@ -221,12 +221,8 @@ get_header(); ?>
 
    <div class="row section-intro">
       <div class="col-twelve">
-
          <h5>Portfolio</h5>
          <h2>Check Out Some of My Works.</h2>
-
-         <!-- 	<p class="lead">Lorem ipsum Do commodo in proident enim in dolor cupidatat adipisicing dolore officia nisi aliqua incididunt Ut veniam lorem ipsum Consectetur ut in in eu do.</p> -->
-
       </div>
    </div> <!-- /section-intro-->
 
@@ -239,216 +235,217 @@ get_header(); ?>
 
             <div class="bgrid folio-item">
                <div class="item-wrap">
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/liberty.jpg" alt="Liberty">
-                  <a href="#modal-01" class="overlay">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/the-ken.png" alt="the-ken">
+                  <a href="#the-ken" class="overlay">
                      <div class="folio-item-table">
                         <div class="folio-item-cell">
-                           <h3 class="folio-title">Liberty</h3>
+                           <h3 class="folio-title">The-Ken</h3>
                            <span class="folio-types">
-                              Graphic Design
+                              The Ken - Business, Startups, Technology and Healthcare news from India.
                            </span>
                         </div>
                      </div>
                   </a>
                </div>
-            </div> <!-- /folio-item -->
+            </div>
 
             <div class="bgrid folio-item">
                <div class="item-wrap">
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/shutterbug.jpg" alt="Shutterbug">
-                  <a href="#modal-02" class="overlay">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/pretestplus.png" alt="Clouds">
+                  <a href="#pretest-plus" class="overlay">
                      <div class="folio-item-table">
                         <div class="folio-item-cell">
-                           <h3 class="folio-title">Shutterbug</h3>
-                           <span class="folio-types">
-                              Web Design
+                           <h3 class="folio-title">Pretest Plus</h3>
+                           <span class="folio-types">CAT, ISEB Pretest, CEM Select &amp; UKiset Online Practice Tests
                            </span>
                         </div>
                      </div>
                   </a>
                </div>
-            </div> <!-- /folio-item -->
+            </div>
 
             <div class="bgrid folio-item">
                <div class="item-wrap">
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/clouds.jpg" alt="Clouds">
-                  <a href="#modal-03" class="overlay">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/walnutedu.png" alt="Shutterbug">
+                  <a href="#walmiki" class="overlay">
                      <div class="folio-item-table">
                         <div class="folio-item-cell">
-                           <h3 class="folio-title">Clouds</h3>
-                           <span class="folio-types">
-                              Web Design
-                           </span>
+                           <h3 class="folio-title">walmiki</h3>
+                           <span class="folio-types">Online learning platform</span>
                         </div>
                      </div>
                   </a>
                </div>
-            </div> <!-- /folio-item -->
+            </div>
 
             <div class="bgrid folio-item">
                <div class="item-wrap">
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/beetle.jpg" alt="Beetle">
-                  <a href="#modal-04" class="overlay">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/officingnow.png" alt="officingnow">
+                  <a href="#officingnow" class="overlay">
                      <div class="folio-item-table">
                         <div class="folio-item-cell">
-                           <h3 class="folio-title">Beetle</h3>
-                           <span class="folio-types">
-                              Branding
-                           </span>
+                           <h3 class="folio-title">Officing Now</h3>
+                           <span class="folio-types">Office Spaces - Ready to Use - Flexible - Monthly, Daily, Hourly Basis</span>
                         </div>
                      </div>
                   </a>
                </div>
-            </div> <!-- /folio-item -->
+            </div>
 
             <div class="bgrid folio-item">
                <div class="item-wrap">
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/lighthouse.jpg" alt="Lighthouse">
-                  <a href="#modal-05" class="overlay">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/exampaperplus.png" alt="exampaperplus">
+                  <a href="#exampaperplus" class="overlay">
                      <div class="folio-item-table">
                         <div class="folio-item-cell">
-                           <h3 class="folio-title">Lighthouse</h3>
+                           <h3 class="folio-title">Exam Papers plus</h3>
                            <span class="folio-types">
-                              Web Development
+                              Buy Practice exam paper 
                            </span>
                         </div>
                      </div>
                   </a>
                </div>
-            </div> <!-- /folio-item -->
+            </div>
 
             <div class="bgrid folio-item">
                <div class="item-wrap">
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/salad.jpg" alt="Salad">
-                  <a href="#modal-06" class="overlay">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/creo.jpg" alt="Creo">
+                  <a href="#creo" class="overlay">
                      <div class="folio-item-table">
                         <div class="folio-item-cell">
-                           <h3 class="folio-title">Salad</h3>
-                           <span class="folio-types">
-                              Branding
-                           </span>
+                           <h3 class="folio-title">Creo</h3>
+                           <span class="folio-types">Smpartphone website</span>
                         </div>
                      </div>
                   </a>
                </div>
-            </div> <!-- /folio-item -->
+            </div>
 
             <!-- modal popups - begin ============================================================= -->
-            <div id="modal-01" class="popup-modal slider mfp-hide">
+            <div id="the-ken" class="popup-modal slider mfp-hide">
 
                <div class="media">
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/modals/m-liberty.jpg" alt="" />
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/modals/m-the-ken.jpg" alt="" />
                </div>
 
                <div class="description-box">
-                  <h4>Liberty</h4>
-                  <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
-
-                  <div class="categories">Web Development</div>
+                  <h4>The-ken</h4>
+                  <p>The Ken is a pioneering re-imagination of business journalism by a team of independent journalists with decades of experience. Worked with The Ken to reimagine the reading experience on screen. The goal of was to bring the rich and engaging reading experience of print magazines to the screen.</p>
+                  <div class="categories"></div>
                </div>
 
                <div class="link-box">
-                  <a href="#">Details</a>
+                  <a href="https://the-ken.com/" target="_blank">See Website</a>
                   <a href="#" class="popup-modal-dismiss">Close</a>
                </div>
 
             </div> <!-- /modal-01 -->
 
-            <div id="modal-02" class="popup-modal slider mfp-hide">
+            <div id="walmiki" class="popup-modal slider mfp-hide">
 
                <div class="media">
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/modals/m-shutterbug.jpg" alt="" />
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/modals/walnutedu-quiz.png" alt="" />
                </div>
 
                <div class="description-box">
-                  <h4>Shutterbug</h4>
-                  <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
+                  <h4>Walmiki</h4>
+                  <p>Walmiki, is an online platform it offers features such as innovative learning videos and quizzes. Features of Walmiki is MCQ based test system. Lakhs of quality questions. Practice mode and class test mode for home and school use. Teacher modules for tracking and analyzing class progress. Chapter based lectures in form of videos. Students can view lectures any number of times. Parents can replay their child’s tests, increasing transparency. Progress over a period can be monitored as test scores are saved.</p>
 
-                  <div class="categories">Web Design</div>
+                  <div class="categories">E-Learning platform</div>
                </div>
 
                <div class="link-box">
-                  <a href="#">Details</a>
+                  <a href="https://walnutedu.net/">See Website</a>
                   <a href="#" class="popup-modal-dismiss">Close</a>
                </div>
 
             </div> <!-- /modal-02 -->
 
-            <div id="modal-03" class="popup-modal slider mfp-hide">
+            <div id="pretest-plus" class="popup-modal slider mfp-hide">
 
                <div class="media">
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/modals/m-clouds.jpg" alt="" />
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/modals/m-pretestplus.png" alt="pretestplus" />
                </div>
 
                <div class="description-box">
-                  <h4>Clouds</h4>
-                  <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
+                  <h4>Pretest Plus</h4>
+                  <p>Pretest Plus is an online platform for students to answer mock tests for the CAT, UKiset and 13+ Pre-test school entrance exams in the UK. Built an e-commerce website to sell the tests and a web app for students to take the tests online.<br/>On the technology side, it consists of:<br/>
+                     <ul>
+                        <li>A custom WooCommerce based eCommerce System.</li>
+                        <li>A PHP backend to manage users, create tests, assign tests, serve and answer tests and analyze the aggregate results.</li>
+                        <li>Dashboards for the Administrator, School Accounts, Parent Account and Student Accounts – with the appropriate level of controls and functionality.</li>
+                        <li>A Mandrill based mailing system to send out notifications and updates about the status of tests.</li>
+                        <li>A comprehensive admin backend with overriding controls for the entire system</li>
+                     </ul></p>
 
-                  <div class="categories">Web Design</div>
+                  <div class="categories">Online Exam Platform</div>
                </div>
 
                <div class="link-box">
-                  <a href="#">Details</a>
+                  <a href="https://pretestplus.co.uk/" target="_blank">See Website</a>
                   <a href="#" class="popup-modal-dismiss">Close</a>
                </div>
 
             </div> <!-- /modal-03 -->
 
-            <div id="modal-04" class="popup-modal slider mfp-hide">
+            <div id="officingnow" class="popup-modal slider mfp-hide">
 
                <div class="media">
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/modals/m-beetle.jpg" alt="" />
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/modals/m-officingnow.png" alt="" />
                </div>
 
                <div class="description-box">
-                  <h4>Beetle</h4>
+                  <h4>Officing <i>Now</i></h4>
                   <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
 
                   <div class="categories">Branding</div>
                </div>
 
                <div class="link-box">
-                  <a href="#">Details</a>
+                  <a href="https://www.officingnow.com/">See Website</a>
                   <a href="#" class="popup-modal-dismiss">Close</a>
                </div>
 
             </div> <!-- /modal-04 -->
 
-            <div id="modal-05" class="popup-modal slider mfp-hide">
+            <div id="exampaperplus" class="popup-modal slider mfp-hide">
 
                <div class="media">
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/modals/m-lighthouse.jpg" alt="" />
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/exampaperplus.png" alt="exampaperplus" />
                </div>
 
                <div class="description-box">
-                  <h4>Lighthouse</h4>
-                  <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
+                  <h4>Exam Papers plus</h4>
+                  <p>Exam Paper Plus is a UK based startup that provides practice papers for competitive academic exams
+as Digital Downloads. It produces engaging and effective practice papers that help prepare students for 7+,
+8+, 9+, 10+, 11+ and 13+ exams across the UK.<br><br><b>On the technology side, it consists of:<br><br></b>Built a clean and bold e-commerce website for them with focus on the offerings and a simple process
+to buy the practice papers as Digital Downloads</p>
 
                   <div class="categories">Web Development</div>
                </div>
 
                <div class="link-box">
-                  <a href="#">Details</a>
+                  <a href="https://exampapersplus.co.uk/" target="_blank">See Website</a>
                   <a href="#" class="popup-modal-dismiss">Close</a>
                </div>
 
             </div> <!-- /modal-05 -->
 
-            <div id="modal-06" class="popup-modal slider mfp-hide">
+            <div id="creo" class="popup-modal slider mfp-hide">
 
                <div class="media">
-                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/modals/m-salad.jpg" alt="" />
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portfolio/modals/m-creo.jpg" alt="" />
                </div>
 
                <div class="description-box">
-                  <h4>Salad</h4>
-                  <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
-
+                  <h4>Creo</h4>
+                  <p>Creo is an ambitious smart devices company with a mission to create better experiences through hardware and software integration. Their first product, the Mark 1, is a smartphone that creates a unique Android experience by building features into the OS and constantly evolving it.</p>
                   <div class="categories">Branding</div>
                </div>
 
                <div class="link-box">
-                  <a href="#">Details</a>
+                  <a href="#">See Website</a>
                   <a href="#" class="popup-modal-dismiss">Close</a>
                </div>
 
